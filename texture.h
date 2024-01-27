d1291bbbebe293bcd851aa3a2603bf6f5f828c3b
z00015463 <zouyingcheng@hihonor.com>
2024-01-27 15:52:26 +0800

    finish chapter 5.7. NOTE: use code in book v3 instead of v4, scaling only apply to the phase `p.z()` to produce the same result as Image 15

--- texture.h
+++ texture.h
@@ -85,8 +85,9 @@ class noise_texture : public texture {
     noise_texture(double sc) : scale(sc) {}
 
     color value(double u, double v, const point3& p) const override {
-        auto s = scale * p;
-        return color(1,1,1) * noise.turb(s);
+        // auto s = scale * p;
+        // return color(1,1,1) * noise.turb(s);
+        return color(1,1,1) * 0.5 * (1 + sin(scale*p.z() + 10*noise.turb(p)));
     }
 
   private:
