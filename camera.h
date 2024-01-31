d4c1be2909937d75c2e6a0cfda9acfe11cb6f3d4
z00015463 <zouyingcheng@hihonor.com>
2024-01-31 10:20:23 +0800

    finish chapter 2.3 part 2. Note: in camera.h the private member `sqrt_spp` and `recip_sqrt_spp` was not introduced. They'are declared and used in the `initialize()` method. See https://github.com/RayTracing/raytracing.github.io/issues/1373

--- camera.h
+++ camera.h
@@ -34,9 +34,11 @@ class camera {
             std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
             for (int i = 0; i < image_width; ++i) {
                 color pixel_color(0, 0, 0);
-                for (int sample = 0; sample < samples_per_pixel; ++sample) {
-                    ray r = get_ray(i, j);
-                    pixel_color += ray_color(r, max_depth, world);
+                for (int s_j = 0; s_j < sqrt_spp; ++s_j) {
+                    for (int s_i = 0; s_i < sqrt_spp; ++s_i) {
+                        ray r = get_ray(i, j, s_i, s_j);
+                        pixel_color += ray_color(r, max_depth, world);
+                    }
                 }
                 write_color(std::cout, pixel_color, samples_per_pixel);
             }
@@ -54,6 +56,8 @@ class camera {
     vec3   u, v, w;        // Camera frame basis vectors
     vec3   defocus_disk_u; // Defocus disk horizontal radius
     vec3   defocus_disk_v; // Defocus disk vertical radius
+    int    sqrt_spp;
+    double recip_sqrt_spp;
 
     void initialize() {
         image_height = static_cast<int>(image_width / aspect_ratio);
@@ -66,6 +70,9 @@ class camera {
         auto h = tan(theta/2);
         auto viewport_height = 2 * h * focus_dist;
         auto viewport_width = viewport_height * (static_cast<double>(image_width)/image_height);
+
+        sqrt_spp = static_cast<int>(sqrt(samples_per_pixel));
+        recip_sqrt_spp = 1.0 / sqrt_spp;
 
         // Calculate the u,v,w unit basis vectors for the camera coordinate frame.
         w = unit_vector(lookfrom - lookat);
@@ -90,12 +97,12 @@ class camera {
         defocus_disk_v = v * defocus_radius;
     }
 
-    ray get_ray(int i, int j) const {
+    ray get_ray(int i, int j, int s_i, int s_j) const {
         // Get a randomly-sampled camera ray for the pixel at location i,j, originating from
-        // the camera defocus disk.
+        // the camera defocus disk, and randomly sampled around the pixel location.
 
         auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-        auto pixel_sample = pixel_center + pixel_sample_square();
+        auto pixel_sample = pixel_center + pixel_sample_square(s_i, s_j);
 
         auto ray_origin = (defocus_angle <= 0) ? center : defocus_disk_sample();
         auto ray_direction = pixel_sample - ray_origin;
@@ -104,10 +111,11 @@ class camera {
         return ray(ray_origin, ray_direction, ray_time);
     }
 
-    vec3 pixel_sample_square() const {
-        // Returns a random point in the square surrounding a pixel at the origin.
-        auto px = -0.5 + random_double();
-        auto py = -0.5 + random_double();
+    vec3 pixel_sample_square(int s_i, int s_j) const {
+        // Returns a random point in the square surrounding a pixel at the origin, given
+        // the two subpixel indices.
+        auto px = -0.5 + recip_sqrt_spp * (s_i + random_double());
+        auto py = -0.5 + recip_sqrt_spp * (s_j + random_double());
         return (px * pixel_delta_u) + (py * pixel_delta_v);
     }
 
